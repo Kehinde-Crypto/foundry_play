68e0ad2b3a369ef670e1d2ebaa42ba343bc601e7
Kehinde-Crypto <kehindebrowns22@gmail.com>
2025-01-21 13:36:01 +0100

    Update NFT.sol

--- src/NFT.sol
+++ src/NFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 import "../lib/forge-std/src/interfaces/IERC721.sol";
-import "../";
+import "../lib/forge-std";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // interface IERC721 {
